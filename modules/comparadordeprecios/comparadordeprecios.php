4e09bc90c85eb7a1ab7c014e65b4ab7c1a20647c
copazo <copazo@exe.cl>
2012-01-17 20:28:26 -0200

    prueba git 6

--- modules/comparadordeprecios/comparadordeprecios.php
+++ modules/comparadordeprecios/comparadordeprecios.php
@@ -189,7 +189,7 @@ class ComparadorDePrecios extends Module
 
 	public function hookProductTab($params)
     {
-		return '<li><a href="#idTab59999" class="idTabHrefShort">Compara precios</a></li>';
+		return '<li><a href="#idTab59999" class="idTabHrefShort">Compara precios 3</a></li>';
 	}
 	
 	public function hookProductTabContent($params)
