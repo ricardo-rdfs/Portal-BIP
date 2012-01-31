fcdc5f7bfb43e365d4ce4591bcf7e4c05a727538
mauricio barria <thiswallz@gmail.com>
2012-01-31 11:49:20 -0300

    configurador

--- wmin/tabs/AdminProducts.php
+++ wmin/tabs/AdminProducts.php
@@ -2954,7 +2954,7 @@ class AdminProducts extends AdminTab
 
 								<img onclick="$(this).prev().search();" style="cursor: pointer;" src="../img/admin/add.gif" alt="'.$this->l('Add an accessory').'" title="'.$this->l('Add an accessory').'" />
 
-<span onclick="addAccessory(\'T de RED ENCORE NE2000 PCI PnP 100MPS FAST ETHERNET PN/ ENL832-TX-RE (ref: ENL832-TX-RE)\',\'421\');" style="cursor: pointer;"><img src="../img/admin/delete.gif" class="middle" alt="" /></span><br />
+<span onclick="addAccessory(\'T de RED ENCORE NE2000 PCI PnP 100MPS FAST ETHERNET PN/ ENL832-TX-RE (ref: ENL832-TX-RE)-421\');" style="cursor: pointer;"><img src="../img/admin/delete.gif" class="middle" alt="" /></span><br />
 
 								<!--<img onclick="$(this).prev().search();" style="cursor: pointer;" src="../img/admin/add.gif" alt="'.$this->l('Add an accessory').'" title="'.$this->l('Add an accessory').'" />-->
 							</div>
