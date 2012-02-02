53e6c8a183a337abf03cd144337e30d910b49ffb
mauricio barria <thiswallz@gmail.com>
2012-02-02 13:05:04 -0300

    test

--- modules/blocklayered/blocklayered.php
+++ modules/blocklayered/blocklayered.php
@@ -2267,7 +2267,7 @@ OR
                             ' LIMIT '.(((int)Tools::getValue('p', 1) - 1) * $n.','.$n));
      
                         }else{
-                            echo "cccccc:::aaaa<br><br>";
+                            echo "aaaaa:::aaaa<br><br>";
                             
                             $n = (int)Tools::getValue('n', Configuration::get('PS_PRODUCTS_PER_PAGE'));
                             $this->products = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS('
